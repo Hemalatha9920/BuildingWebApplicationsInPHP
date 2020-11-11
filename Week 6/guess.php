239319571736e52e85788304c869b4fd7c721636
Hemalatha9920 <74291833+Hemalatha9920@users.noreply.github.com>
2020-11-11 20:40:06 +0530

    Update guess.php

--- Week 6/guess.php
+++ Week 6/guess.php
@@ -1,10 +1,10 @@
 <html>
 <body>
-<title> Anamitra Musib 41884c79 </title>
+<title>  83db1518 </title>
 <h1> Welcome to my guessing game</h1>
 
 <?php
-$correctnumber=26; //change this number //
+$correctnumber=75; //change this number //
 if(isset($_GET['guess']))
 {
   if (is_numeric($_GET['guess'])===FALSE)
